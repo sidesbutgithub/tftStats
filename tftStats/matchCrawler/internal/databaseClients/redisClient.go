ccfd4c78e1f8a43efc08ae6e0c39515907e8c52d
sidesbutgithub <SidneyWai3@gmail.com>
2025-07-29 16:12:35 -0400

    Add 'tftStats/matchCrawler/' from commit '7518c11d22e92d15f71f96e8c0cd5fa01ae3cc47'
    git-subtree-dir: tftStats/matchCrawler
    git-subtree-mainline: 7518c11d22e92d15f71f96e8c0cd5fa01ae3cc47
    git-subtree-split: 7518c11d22e92d15f71f96e8c0cd5fa01ae3cc47

---
+++ tftStats/matchCrawler/internal/databaseClients/redisClient.go
@@ -0,0 +1,161 @@
+package databaseClients
+
+import (
+	"context"
+	"errors"
+	"log"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type RedisDB struct {
+	Client  *redis.Client
+	Context context.Context
+	Timeout time.Duration
+}
+
+//Connect to Redis Database
+
+func (db *RedisDB) ConnectRedis(connString string) {
+	opt, err := redis.ParseURL(connString)
+	if err != nil {
+		log.Print(err)
+		log.Fatal("error parsing connection string")
+	}
+
+	db.Client = redis.NewClient(opt)
+	db.Context = context.Background()
+	err = db.Client.Ping(db.Context).Err()
+	if err != nil {
+		log.Fatal("Failed to connect to Redis")
+	}
+}
+
+func (db *RedisDB) SetTimeout(blockingTimeout time.Duration) {
+	db.Timeout = blockingTimeout
+}
+
+//Set for visited Players
+
+func (db *RedisDB) MarkPlayerVisited(Puuid string) error {
+	if db.Client == nil {
+		return errors.New("database not connected")
+	}
+	err := db.Client.SAdd(db.Context, "visitedPlayers", Puuid).Err()
+	return err
+}
+
+func (db *RedisDB) CheckPlayerVisited(Puuid string) (bool, error) {
+	if db.Client == nil {
+		return false, errors.New("database not connected")
+	}
+	visited, err := db.Client.SIsMember(db.Context, "visitedPlayers", Puuid).Result()
+	if err != nil {
+		return false, err
+	}
+	return visited, err
+}
+
+//Set for visited matches
+
+func (db *RedisDB) MarkMatchVisited(matchId string) error {
+	if db.Client == nil {
+		return errors.New("database not connected")
+	}
+	err := db.Client.SAdd(db.Context, "visitedMatches", matchId).Err()
+	if err != nil {
+		log.Print("Failed to write match to visited set")
+		return err
+	}
+	return nil
+}
+
+func (db *RedisDB) CheckMatchVisited(matchId string) (bool, error) {
+	if db.Client == nil {
+		return false, errors.New("database not connected")
+	}
+	visited, err := db.Client.SIsMember(db.Context, "visitedMatches", matchId).Result()
+	if err != nil {
+		return false, err
+	}
+	return visited, err
+}
+
+// Queue for players to visit
+func (db *RedisDB) EnqueuePlayer(Puuid string) error {
+	if db.Client == nil {
+		return errors.New("database not connected")
+	}
+	err := db.Client.LPush(db.Context, "playersQueue", Puuid).Err()
+	return err
+}
+
+func (db *RedisDB) DequeuePlayer() (string, error) {
+	if db.Client == nil {
+		return "", errors.New("database not connected")
+	}
+	queueLen, err := db.Client.LLen(db.Context, "playersQueue").Result()
+	if err != nil {
+		return "", err
+	}
+	if queueLen == 0 {
+		log.Print("players queue is empty, waiting for players to be added")
+	}
+	puuid, err := db.Client.BRPop(db.Context, db.Timeout, "playersQueue").Result()
+	if err != nil {
+		return "", err
+	}
+	if queueLen == 0 {
+		log.Print("players queue no longer empty, successfully popped value")
+	}
+	return puuid[1], err
+}
+
+// Queue for matches to visit
+func (db *RedisDB) EnqueueMatch(MatchId string) error {
+	if db.Client == nil {
+		return errors.New("database not connected")
+	}
+	err := db.Client.LPush(db.Context, "matchesQueue", MatchId).Err()
+	return err
+}
+
+func (db *RedisDB) DequeueMatch() (string, error) {
+	if db.Client == nil {
+		return "", errors.New("database not connected")
+	}
+	queueLen, err := db.Client.LLen(db.Context, "matchesQueue").Result()
+	if err != nil {
+		return "", err
+	}
+	if queueLen == 0 {
+		log.Print("matches queue is empty, waiting for matches to be added")
+	}
+	matchId, err := db.Client.BRPop(db.Context, db.Timeout, "matchesQueue").Result()
+	if err != nil {
+		return "", err
+	}
+	if queueLen == 0 {
+		log.Print("players queue no longer empty, successfully popped value")
+	}
+	return matchId[1], err
+}
+
+func (db *RedisDB) PlayersQueueLen() (int, error) {
+	queueLen, err := db.Client.LLen(db.Context, "playersQueue").Result()
+	if err != nil {
+		log.Print("error in getting length of player queue")
+		return 0, err
+	}
+	return int(queueLen), nil
+}
+
+func (db *RedisDB) MatchesQueueLen() (int, error) {
+	queueLen, err := db.Client.LLen(db.Context, "matchesQueue").Result()
+	if err != nil {
+		log.Print("error in getting length of matches queue")
+		return 0, err
+	}
+	return int(queueLen), nil
+}
